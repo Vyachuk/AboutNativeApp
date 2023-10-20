e16bcf7e59d210653040b840096c84dd358a2d39
Vyachuk <Slavkoka045@gmail.com>
2023-10-20 20:39:24 +0300

    ADD: Post screen navigation

---
+++ screens/Main/PostsScreen/EntryPostsScreen/EntryPostsScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const EntryPostsScreen = () => {
+  return (
+    <View>
+      <Text>Entry Screen</Text>
+    </View>
+  );
+};
+
+export default EntryPostsScreen;
--- screens/Main/PostsScreen/PostsScreen.js
+++ screens/Main/PostsScreen/PostsScreen.js
@@ -1,11 +1,51 @@
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native";
+import EntryPostScreen from "./EntryPostsScreen/EntryPostsScreen";
+import CommentsScreen from "./CommentsScreen/CommentsScreen";
+import MapScreen from "./MapScreen/MapScreen";
+import { colors } from "../../../constants/globalThemeConstants";
+import { Feather } from "@expo/vector-icons";
+
+const PostsStack = createStackNavigator();
 
 const PostsScreen = () => {
   return (
-    <View>
-      <Text>Posts Screen</Text>
-    </View>
+    <PostsStack.Navigator
+      initialRouteName="PostsDefault"
+      screenOptions={{ headerTitleAlign: "center" }}
+    >
+      <PostsStack.Screen
+        name="Entry"
+        component={EntryPostScreen}
+        options={{
+          title: "Публікації",
+          headerRight: () => (
+            <TouchableOpacity
+              style={{ marginRight: 16 }}
+              activeOpacity={0.5}
+              // onPress={handleSignOut}
+            >
+              <Feather name="log-out" size={24} color={colors.textColor} />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+      <PostsStack.Screen
+        name="Comments"
+        component={CommentsScreen}
+        options={{
+          title: "Коментарі",
+        }}
+      />
+      <PostsStack.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          title: "Мапа",
+        }}
+      />
+    </PostsStack.Navigator>
   );
 };
 
