8aeba6e3dda52ae17c931ef1d81bb0683985ef38
Vyachuk <Slavkoka045@gmail.com>
2023-10-21 14:56:14 +0300

    ADD: Post Screen + mock data

---
+++ components/PostItem.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { Feather, FontAwesome5, FontAwesome } from "@expo/vector-icons";
+
+import { useNavigation } from "@react-navigation/native";
+import { colors, FontFamily } from "../constants/globalThemeConstants";
+
+export const PostItem = ({ post }) => {
+  const { id, image, location, title, comments } = post;
+
+  const { navigate } = useNavigation();
+
+  const handleCommentsClick = () => {
+    navigate("Comments");
+  };
+
+  const handleLocation = () => {
+    navigate("Map");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.descriptionContainer}>
+        <View style={styles.btnContainer}>
+          <TouchableOpacity
+            style={styles.commentsBtn}
+            activeOpacity={0.5}
+            onPress={handleCommentsClick}
+          >
+            {comments ? (
+              <FontAwesome
+                name="comment"
+                size={24}
+                color={colors.orange}
+                style={{
+                  transform: [{ scaleX: -1 }],
+                }}
+              />
+            ) : (
+              <FontAwesome5
+                name="comment"
+                size={24}
+                color={colors.gray}
+                style={{
+                  transform: [{ scaleX: -1 }],
+                }}
+              />
+            )}
+            <Text
+              style={{
+                ...styles.commentsNumber,
+                color: comments ? colors.textColor : colors.gray,
+              }}
+            >
+              {comments}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity activeOpacity={0.5} style={styles.commentsBtn}>
+            <Feather name="thumbs-up" size={24} color={colors.orange} />
+            <Text
+              style={{
+                ...styles.commentsNumber,
+                color: colors.textColor,
+              }}
+            >
+              {20}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={styles.locationBtn}
+          activeOpacity={0.5}
+          onPress={handleLocation}
+        >
+          <Feather name="map-pin" size={24} color={colors.gray} />
+          <Text style={styles.locationTitle}>{location}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    gap: 8,
+    marginBottom: 32,
+  },
+  image: {
+    height: 240,
+    borderRadius: 8,
+    objectFit: "cover",
+    width: "100%",
+  },
+  title: {
+    fontFamily: FontFamily.robotoMedium,
+    fontSize: 16,
+    color: colors.textColor,
+  },
+  descriptionContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 8,
+  },
+  btnContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 16,
+  },
+  commentsBtn: {
+    flexDirection: "row",
+    gap: 6,
+    alignItems: "center",
+  },
+  commentsNumber: {
+    fontFamily: FontFamily.robotoRegular,
+    fontSize: 16,
+  },
+  locationBtn: {
+    maxWidth: 230,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 4,
+  },
+  locationTitle: {
+    fontFamily: FontFamily.robotoRegular,
+    fontSize: 16,
+    color: colors.textColor,
+    textDecorationLine: "underline",
+  },
+});
--- screens/Main/PostsScreen/EntryPostsScreen/EntryPostsScreen.js
+++ screens/Main/PostsScreen/EntryPostsScreen/EntryPostsScreen.js
@@ -1,18 +1,84 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { PostItem } from "../../../../components/PostItem";
+import { FontFamily } from "../../../../constants/globalThemeConstants";
+const POSTS = [
+  {
+    image: require("../../../../assets/mock/forest.webp"),
+    title: "Forest",
+    location: "Lviv",
+    comments: "2",
+    id: 0,
+  },
+  {
+    image: require("../../../../assets/mock/apache.jpeg"),
+    title: "Terapak",
+    location: "Kyiv",
+    comments: "44",
+    id: 1,
+  },
+  {
+    image: require("../../../../assets/mock/tundra.jpeg"),
+    title: "Tundra",
+    location: "Sahara",
+    comments: "22",
+    id: 2,
+  },
+];
 
 const EntryPostsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Post Screen</Text>
+      <View style={styles.ownerContainer}>
+        <Image
+          source={require("../../../../assets/woman.png")}
+          style={styles.image}
+        />
+        <View style={styles.textWrapper}>
+          <Text style={styles.title}>Natali Romanova</Text>
+          <Text style={styles.mail}>email@example.com</Text>
+        </View>
+      </View>
+      <View style={styles.listWrapper}>
+        <FlatList
+          data={POSTS}
+          renderItem={({ item }) => <PostItem post={item} />}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignSelf: "center",
+    paddingHorizontal: 16,
+    paddingVertical: 32,
+  },
+  ownerContainer: {
+    flexDirection: "row",
+    gap: 8,
+    marginBottom: 32,
+  },
+  listWrapper: {
+    marginBottom: 48,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 16,
+  },
+  textWrapper: {
     justifyContent: "center",
+    height: 60,
+  },
+  title: {
+    fontFamily: FontFamily.robotoBold,
+    fontSize: 13,
+  },
+  mail: {
+    fontSize: 11,
   },
 });
 
