a5344c4ecdf2dee5bd70a21eac2e478edad4b84c
Vyachuk <Slavkoka045@gmail.com>
2023-10-24 23:49:33 +0300

    EDIT: method uploading photo

--- screens/Auth/useAuthHook.js
+++ screens/Auth/useAuthHook.js
@@ -1,17 +1,23 @@
 import { signInThunk, signUpThunk } from "../../redux/Auth/authOperation";
 import { useDispatch } from "react-redux";
+import { uploadImageToServer } from "../../utils/uploadImageToServer.js";
 
 export default function useAuthLogicHook({ creds, profile, screen }) {
   const dispatch = useDispatch();
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (!(creds.email && creds.password)) return;
     if (screen === "LoginScreen") dispatch(signInThunk(creds));
     if (screen === "RegistrationScreen" && profile?.name) {
+      const imageUrl = await uploadImageToServer({
+        imageUri: profile.photo,
+        folder: "avatars",
+      });
+      console.log(imageUrl);
       dispatch(
         signUpThunk({
           name: profile.name,
-          photo: profile.photo,
+          photo: imageUrl,
           ...creds,
         })
       );
